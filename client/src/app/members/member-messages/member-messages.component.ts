34c9f460c7645b30164465689fb6ad243649a683
Bane1110 <brankaccc@gmail.com>
2023-12-09 17:07:35 +0100

    Message hub

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -11,10 +11,9 @@ import { MessageService } from 'src/app/_services/message.service';
 export class MemberMessagesComponent {
   @ViewChild('messageForm') messageForm?: NgForm;
   @Input() username?: string;
-  @Input() messages: Message[] = [];
   messageContent = '';
 
-  constructor(private messageService: MessageService) {}
+  constructor(public messageService: MessageService) {}
 
   sendMessage() {
     if (!this.username) return;
@@ -22,8 +21,8 @@ export class MemberMessagesComponent {
       .sendMessage(this.username, this.messageContent)
       .subscribe({
         next: (message) => {
-          this.messages.push(message);
-          this.messageForm?.reset();
+          // this.messages.push(message);
+          // this.messageForm?.reset();
         },
       });
   }
