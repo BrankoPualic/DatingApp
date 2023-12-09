ebdb292d504992a808cf8e12f61684eed90440b2
Bane1110 <brankaccc@gmail.com>
2023-12-09 13:28:41 +0100

    Admin panel component, Admin guard, has-role custom directive

---
+++ client/src/app/_guards/admin.guard.ts
@@ -0,0 +1,27 @@
+import { Observable, map } from 'rxjs';
+import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AdminGuard {
+  constructor(
+    private accountService: AccountService,
+    private toastr: ToastrService
+  ) {}
+  canActivate(): Observable<boolean> {
+    return this.accountService.currentUser$.pipe(
+      map((user) => {
+        if (!user) return false;
+        if (user.roles.includes('Admin') || user.roles.includes('Moderator')) {
+          return true;
+        } else {
+          this.toastr.error('You cannot enter this area');
+          return false;
+        }
+      })
+    );
+  }
+}
